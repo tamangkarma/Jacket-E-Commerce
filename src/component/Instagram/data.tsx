15e5e263bb3aa80c882e3e0589f2900b98f428a1
Karma Tamang <tamangkarma160@gmail.com>
2025-07-23 20:38:25 +0545

    chore: images updated

--- src/component/Instagram/data.tsx
+++ src/component/Instagram/data.tsx
@@ -1,22 +1,22 @@
 export const instaData = [
     {
         id:1 ,
-        image: "/public/Instagram/Paste image.svg",
+        image: "/Instagram/Paste image.svg",
         alt: "Instagram Post 1"
     },
     {
         id:2,
-        image: "/public/Instagram/2.svg",
+        image: "/Instagram/2.svg",
         alt: "Instagram Post 2"
     },
     {
         id:3,
-        image: "/public/Instagram/3.svg",
+        image: "/Instagram/3.svg",
         alt: "Instagram Post 3"
     },
     {
         id:4,
-        image: "/public/Instagram/4.svg",
+        image: "/Instagram/4.svg",
         alt: "Instagram Post 4"
     },
 ]
--- src/data/valueData.ts
+++ src/data/valueData.ts
@@ -3,24 +3,24 @@ export const valueData = [
         id:1,
         title: "Free Shipping",
         description: "Order above $200",
-        image: "/public/logo/fast delivery.svg"
+        image: "/logo/fast delivery.svg"
     },
     {
         id:2,
         title: "Money Back",
         description: "30 days guarantee",
-        image: "/public/logo/money.svg"
+        image: "/logo/money.svg"
     },
     {
         id:3,
         title: "Secure Payments",
         description: "Secured by Stripe",
-        image: "/public/logo/lock 01.svg"
+        image: "/logo/lock 01.svg"
     },
     {
         id:4,
         title: "Support 24/7",
         description: "Phone and Email support",
-        image: "/public/logo/call.svg"
+        image: "/logo/call.svg"
     },
 ]

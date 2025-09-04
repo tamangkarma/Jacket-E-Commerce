c0e2695858996288e54d43371e561d1d84e11f11
Karma Tamang <tamangkarma160@gmail.com>
2025-09-04 19:35:59 +0545

    chore: updated

--- src/component/Header/myAccount/account.tsx
+++ src/component/Header/myAccount/account.tsx
@@ -5,6 +5,7 @@ const Account = () => {
   
   return (
     <section className="flex flex-col my-4 justify-center items-center md:px-[160px]">
+      <h1>Profile</h1>
       <ProfileSidebar/>
     </section>
   );
